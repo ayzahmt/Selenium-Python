50533ae01eca7a2e3523a76fd4adfad8722c2f3f
Ahmet <ayazahmet34@gmail.com>
2021-01-10 16:15:28 +0300

    locating elements

---
+++ intro.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get('https://ayzahmt.github.io')
+print(driver.title)
+#driver.close()
+#driver.quit()
